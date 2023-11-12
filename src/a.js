e5ba3181f6dda0147c797168f9f67a0ba6ba11d4
power <1410837981@qq.com>
2023-11-12 22:27:30 +0800

    feat: webpack-dev-server

--- src/a.js
+++ src/a.js
@@ -1,4 +1,5 @@
-import('./b.js');
+import { name } from './b'
+console.log(name)
 import './b.css'
 let add = (a, b) => a + b
 console.log(add(3, 5))
@@ -8,8 +9,9 @@ import img1 from './sky1.jpg'
 console.log(img)
 console.log(img1)
 window.onload = () => {
-  let dom = `<img src="${img}">`
-  let dom1 = `<img src="${img1}">`
-  document.getElementById('main').innerHTML = dom
-  document.getElementById('main1').innerHTML = dom1
+  // let dom = `<img src="${img}">`
+  // let dom1 = `<img src="${img1}">`
+  // document.getElementById('main').innerHTML = dom
+  // document.getElementById('main1').innerHTML = dom1
+  console.log(2221511)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 清除
+const CopyPlugin = require('copy-webpack-plugin'); // 复制资源
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'none',
   context: path.resolve(__dirname, './src'), //基础目录
@@ -6,8 +9,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), //输出目录 
     filename: 'bundle.js', // 文件名称
-    publicPath: './dist/', // 访问静态资源路径
-    chunkFilename: '[chunkhash:8].js', // 异步模块名称
+    publicPath: '', // 访问静态资源路径
+    // chunkFilename: '[name]-[chunkhash:8].js', // 异步模块名称
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: './',
+    compress: true,
+    hot: true,
+    port: 8080
   },
   module: {
     rules: [
@@ -18,6 +28,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        include: /src/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -31,11 +42,31 @@ module.exports = {
         use: {
           loader: 'url-loader',
           options: {
-            limit: 8 * 1024
-          }
+            limit: 8 * 1024,
+            name: '[name]-[contenthash:8].[ext]',
+            publicPath: ''
+          },
+         
         }
        
       }
     ]
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'webpack和babel入门教材', // html 标题
+      filename: 'index.html',
+      minify: true, // 是否压缩生成的html文件
+      showErrors: false, // 是否在html上展示错误信息
+    }),
+    new CleanWebpackPlugin(), // 清除本地文件
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/img/'),
+          to: path.resolve(__dirname, 'dist/img/')
+        }
+      ]
+    })
+  ]
 }
